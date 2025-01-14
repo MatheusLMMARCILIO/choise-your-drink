7a9bfb559b844bf6a0a361032c9ffd4d055c1186
MatheusLMMARCILIO <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2025-01-14 16:25:37 -0300

    ok

--- src/Sayian.jsx
+++ src/Sayian.jsx
@@ -5,16 +5,17 @@ import { useState } from "react";
 import './Sayian.css';
 
 function Sayian() {
-  const star = './IMAGES/stars.webp';
+  const star = '/IMAGES/stars.webp';
 
   const flavourChoice = [
     { id: '1', span: 'PEACH', IMG: '/IMAGES/peach_flavour.webp', color: '#f98a03', soda: '/IMAGES/dragon__.webp' },
-    { id: '2', span: 'LIME', IMG: '/IMAGES/lime_flavour.webp', color: '#69ac0b', soda: '/IMAGES//lime__.webp' },
+    { id: '2', span: 'LIME', IMG: '/IMAGES/lime_flavour.webp', color: '#69ac0b', soda: '/IMAGES/lime__.webp' },
     { id: '3', span: 'DRAGONFRUIT', IMG: '/IMAGES/dragon_flavour.webp', color: '#ff21ce', soda: '/IMAGES/dragon__.webp' },
     { id: '4', span: 'LEMON', IMG: '/IMAGES/lemon_flavour.webp', color: '#dfc129', soda: '/IMAGES/lemon__.webp' },
     { id: '5', span: 'STRAWBERRY', IMG: '/IMAGES/strawberry_flavour.webp', color: '#e0241d', soda: '/IMAGES/strawberry__.webp' },
     { id: '6', span: 'GRAPE', IMG: '/IMAGES/grape_flavour.webp', color: '#6b32ce', soda: '/IMAGES/grape__.webp' },
   ];
+  
 
 
   const [flavour, setFlavour] = useState(flavourChoice[0].span); 
