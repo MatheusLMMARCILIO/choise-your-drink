fb6f86bd37b78d1895e00c03ce6f971306595f52
MatheusLMMARCILIO <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2025-01-14 16:13:27 -0300

    ok

--- src/Sayian.jsx
+++ src/Sayian.jsx
@@ -8,12 +8,12 @@ function Sayian() {
   const star = 'public/IMAGES/stars.webp';
 
   const flavourChoice = [
-    { id: '1', span: 'PEACH', IMG: 'public/IMAGES/peach_flavour.webp', color: '#f98a03', soda: 'public/IMAGES/dragon__.webp' },
-    { id: '2', span: 'LIME', IMG: 'public/IMAGES/lime_flavour.webp', color: '#69ac0b', soda: 'public/IMAGES//lime__.webp' },
-    { id: '3', span: 'DRAGONFRUIT', IMG: 'public/IMAGES/dragon_flavour.webp', color: '#ff21ce', soda: 'public/IMAGES/dragon__.webp' },
-    { id: '4', span: 'LEMON', IMG: 'public/IMAGES/lemon_flavour.webp', color: '#dfc129', soda: 'public/IMAGES/lemon__.webp' },
-    { id: '5', span: 'STRAWBERRY', IMG: 'public/IMAGES/strawberry_flavour.webp', color: '#e0241d', soda: 'public/IMAGES/strawberry__.webp' },
-    { id: '6', span: 'GRAPE', IMG: 'public/IMAGES/grape_flavour.webp', color: '#6b32ce', soda: 'public/IMAGES/grape__.webp' },
+    { id: '1', span: 'PEACH', IMG: './public/IMAGES/peach_flavour.webp', color: '#f98a03', soda: 'public/IMAGES/dragon__.webp' },
+    { id: '2', span: 'LIME', IMG: './public/IMAGES/lime_flavour.webp', color: '#69ac0b', soda: 'public/IMAGES//lime__.webp' },
+    { id: '3', span: 'DRAGONFRUIT', IMG: './public/IMAGES/dragon_flavour.webp', color: '#ff21ce', soda: 'public/IMAGES/dragon__.webp' },
+    { id: '4', span: 'LEMON', IMG: './public/IMAGES/lemon_flavour.webp', color: '#dfc129', soda: 'public/IMAGES/lemon__.webp' },
+    { id: '5', span: 'STRAWBERRY', IMG: './public/IMAGES/strawberry_flavour.webp', color: '#e0241d', soda: 'public/IMAGES/strawberry__.webp' },
+    { id: '6', span: 'GRAPE', IMG: './public/IMAGES/grape_flavour.webp', color: '#6b32ce', soda: 'public/IMAGES/grape__.webp' },
   ];
 
 
